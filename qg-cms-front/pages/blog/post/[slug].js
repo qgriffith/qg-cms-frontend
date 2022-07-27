6f28b4800450273f44d981c6fce3b632a4c5f41f
Quenten Griffith <qgriffith@gmail.com>
2022-07-27 19:36:04 -0400

    Add queryapi helper

--- qg-cms-front/pages/blog/post/[slug].js
+++ qg-cms-front/pages/blog/post/[slug].js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import getStrapiURL from "../../../lib/GetStrapiURL"
 import PostLayout from "../../../layouts/PostLayout"
-import qs from "qs"
+import { queryAPI } from "../../../lib/QueryAPI"
 
 // router is required for fallback: true
 const Article = ({ post }) => {
@@ -21,19 +21,25 @@ const Article = ({ post }) => {
   export default Article
   
   export async function getStaticProps({ params }) {
-    
-    const res = await axios.request(getStrapiURL(`/api/articles/?populate[writer][populate]=*&?filters[slug]=${params.slug}`));
-    const post = await res.data.data[0]
+    const post = await queryAPI('/articles', {
+      filters: {
+        slug: params.slug
+      },
+      populate: {
+        writer: {
+          populate: "*",
+        },
+      },
+    })
     return {
       props: {
-        post,
+        post: post.data[0],
       },
     }
   }
 
   export async function getStaticPaths() {
-    const res = await axios.request(getStrapiURL("/api/articles"))
-    const posts = await res.data
+    const posts = await queryAPI("/articles")
     const paths = posts.data.map((post) => ({ params: { slug: post.attributes.slug} }))
     return {
       paths,

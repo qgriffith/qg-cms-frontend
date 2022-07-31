389e9cb9cb874c3c5460b1437569327e348d881a
Quenten Griffith <qgriffith@gmail.com>
2022-07-31 15:14:34 -0400

    fix build issues

--- qg-cms-front/components/Tags.js
+++ qg-cms-front/components/Tags.js
@@ -22,7 +22,7 @@ export default function Tags({id}) {
   
       return (
         tags.map(tag =>
-           <Link href={`/articles/tag/${tag.attributes.name}`}>
+           <Link key={tag.id} href={`/articles/tag/${tag.attributes.name}`}>
             <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                 {tag.attributes.name}
             </a>

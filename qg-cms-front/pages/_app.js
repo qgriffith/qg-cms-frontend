3dfee3ccff67846c5a296fff60c88b3249cdc372
Quenten Griffith <qgriffith@gmail.com>
2022-08-03 10:14:09 -0400

    added seo and pulling title, logo and favoicon from strapi

--- qg-cms-front/pages/_app.js
+++ qg-cms-front/pages/_app.js
@@ -1,21 +1,46 @@
 import '../styles/globals.css'
 import '../styles/prism.css'
 import { ThemeProvider } from 'next-themes'
+import App from "next/app"
 import Head from 'next/head'
 import Layout from '../layouts/Layout'
+import { queryAPI } from '../lib/QueryAPI'
+import { createContext } from "react"
+import getStrapiMedia from '../lib/GetStrapiMedia'
 
 function MyApp({ Component, pageProps }) {
+  const { global } = pageProps
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>{global.attributes.defaultSEO.metaTitle}</title>
+        <meta property="og:title" content={global.attributes.defaultSEO.metaTitle} />
+        <meta property="og:description" content={global.attributes.defaultSEO.metaDescription} />
+        <meta name="description" content={global.attributes.defaultSEO.metaDescription} />
+        <meta name="image" content={getStrapiMedia(global.attributes.siteLogo.data.attributes.url)} />
+        <meta name="og:image" content={getStrapiMedia(global.attributes.siteLogo.data.attributes.url)} />
+        <link rel="shortcut icon" href={getStrapiMedia(global.attributes.favicon.data.attributes.url)} />
       </Head>
-      <Layout>
+      <Layout global={global}>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
   )
   
 }
+
+MyApp.getInitialProps = async (ctx) => {
+ // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(ctx)
+  // Fetch global site settings from Strapi
+  const global = await queryAPI('/global', {
+      populate: "*"
+    }
+  )
+  return { ...appProps, pageProps: { global: global.data } }
+}
+
+
 
 export default MyApp
